c8c6126a355ec83c9f11406c7bba0c12bec90092
Ubuntu <ubuntu@ip-172-31-26-17.us-west-2.compute.internal>
2019-11-16 14:29:21 +0000

    16/11

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,8 +1,15 @@
-variable "aws_region" {default = "us-east-1"}
+variable "aws_region" {default = "us-west-2"}
 variable "count" {}
 variable "cred_file" { default = "/var/jenkins_home/aws_keys/credentials" }
 variable "instanceType" {}
-variable "aws_vpc" {}
+variable "aws_vpc" { 
+  cidr_block ="10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags {
+    Name = "terraform_aws_vpc_try"
+  }
+}
 variable "instanceName" {}
 variable "serviceName" {}
 variable "envName" {}
